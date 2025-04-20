62378c54750276640ef2c304fcc536d93da772cd
tors1 <dm8990090@gmail.com>
2025-04-20 17:29:14 +0530

    - Added two random number generator classes, one for dice roll, other for custom origin and max value. - Switched output method in randomno.java from print statements to JOptionPane.showMessageDialog.

--- tung tung sahur/src/main.java
+++ tung tung sahur/src/main.java
@@ -9,11 +9,12 @@ class main {
         boolean run=true;
         pu.ps("Hello, This is a test program!");
         //pu.print("[number]program_name");
-        pu.ps("Would you like to use " +"\n"+"[0]menu driven program1");
+        pu.ps("Would you like to use " +"\n"+"[0]terminal* menu driven program1");  //tors1
         //pu.ps("Would you like to use"+"[1] menu-driven program2"+"?");
-        pu.ps("[2] Run python script");
-        pu.ps("[3] GUI");
-        pu.ps("[4] Random Number Generator");
+        pu.ps("[2] Run python script"); //tors1
+        pu.ps("[3] GUI"); //tors1
+        pu.ps("[4] Roll a dice"); //tors1
+        pu.ps("[5] Random no. generator"); //tors1
         pu.ps("Enter your choice: ");
         int c = sc.nextInt();
         switch (c) { //DO NOT SWITCH TO IF ELSE.
@@ -32,6 +33,9 @@ class main {
                 break;
             case 4:
                 randomno.dice();
+                break;
+            case 5:
+                randomno.otherandom();
                 break;
             default:
                 pu.ps("invalid choice, exiting");
--- tung tung sahur/src/randomno.java
+++ tung tung sahur/src/randomno.java
@@ -1,8 +1,17 @@
+import javax.swing.*;
 import java.util.Random;
 public class randomno {
     static void dice(){
         Random rand=new Random();
         int a=rand.nextInt(1,7);
-        pu.pI(a);
+        JOptionPane.showMessageDialog(null,"The roll of a dice gives you "+a);
     }
+    static void otherandom(){
+        Random rand=new Random();
+        int a= Integer.parseInt(JOptionPane.showInputDialog("Enter Lowest Digit"));
+        int b= Integer.parseInt(JOptionPane.showInputDialog("Enter Highest Digit"));
+        int c=rand.nextInt(a,(b+1));
+        JOptionPane.showMessageDialog(null,"Random Number:"+c);
+    }
+
 }
